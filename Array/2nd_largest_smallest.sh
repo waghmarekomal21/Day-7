52fd0b7d337373dd02ab4d8baef74d9dc1273cf3
Komal <waghmarekomal21@gmail.com>
2022-08-06 00:56:52 +0530

    Programs

---
+++ Array/2nd_largest_smallest.sh
@@ -0,0 +1,43 @@
+function generateRandomNumber() {
+randomNumber=$((RANDOM%1000))
+echo $randomNumber
+}
+function findSecondLargest() {
+randomNumbers=("$@")
+largestNumber=${randomNumbers[0]}
+secondLargestNumber=${randomNumbers[0]}
+smallestNumber=${randomNumbers[0]}
+secondSmallestNumber=${randomNumbers[0]}
+length="${#randomNumbers[@]}"
+for ((counter=1; counter < $length ; counter++)) 
+do
+if [ $largestNumber -lt ${randomNumbers[$counter]} ]
+then
+secondLargestNumber=$largestNumber 
+largestNumber=${randomNumbers[$counter]}
+elif [ $secondLargestNumber -lt  ${randomNumbers[$counter]} ]
+then 
+secondLargestNumber=${randomNumbers[$counter]}
+fi
+if [ $smallestNumber -gt ${randomNumbers[$counter]} ]
+then
+secondSmallestNumber=$smallestNumber
+smallestNumber=${randomNumbers[$counter]}
+elif [ $secondSmallestNumber -gt  ${randomNumbers[$counter]} ]
+then
+secondSmallestNumber=${randomNumbers[$counter]}
+fi
+done
+echo "Second Largest number is $secondLargestNumber"
+echo "Second Smallest number is $secondSmallestNumber"
+}
+function storeRandomNumber() {
+counter=0
+while [ $counter -lt 10 ]
+do
+randomNumberArray[((counter++))]=$(generateRandomNumber)
+done
+echo ${randomNumberArray[@]}	
+findSecondLargest ${randomNumberArray[@]} 
+}
+storeRandomNumber
---
+++ Array/prime_factor.sh
@@ -0,0 +1,13 @@
+read -p "Enter the number to find factors:" number
+declare a primeFactors
+counter=0
+primeFactorsOf=$number
+for ((count=2 ; count<= $number ; count++ ))
+do
+while (($number%$count == 0 ))
+do
+primeFactors[((counter++))]=$count
+number=$((number/count))
+done
+done
+echo "Prime factors of $primeFactorsOf is ${primeFactors[@]}"
